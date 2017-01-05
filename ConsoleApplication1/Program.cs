4bd9dedb0d72c53ee567def15663a64dda3dc808
hp <hp@HP-PC>
2017-01-05 10:37:42 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program { 
+
+
+       
+        static void Main(string[] args)
+        {
+             string s = "man";
+            Console.WriteLine(s.Length);
+            char z;
+            for (int i=s.Length-1;i>=0;i--)
+            {
+                z = s[i];
+                Console.Write(z);
+            }
+            Console.ReadKey();
+        
+            
+           
+
+        }
+    }
+}
